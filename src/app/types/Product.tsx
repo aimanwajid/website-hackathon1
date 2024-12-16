dd77ec861d57d700eaca070392502264ee78bc47
aimanwajid <salmawajid003@gmail.com>
2024-12-16 09:04:44 -0800

    first commit

---
+++ src/app/types/Product.tsx
@@ -0,0 +1,10 @@
+export interface Product {
+    id: string;
+    name: string;
+    category: string;
+    price: number;
+    colors: number;
+    images: string[]; // Array of image URLs
+    label?: string;
+  }
+  
